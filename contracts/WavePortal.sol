9e8da95fcec379887effc4bb5c2dbf196ab64c80
Rares Stanciu <rares.stanciu@outlook.com>
2021-11-21 15:23:35 +0200

    add wave portal contract and deployment script

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+  uint256 totalWaves;
+
+  constructor() {
+    console.log("Yo yo, I am a contract and I am smart.");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have a total of %d waves so far.", totalWaves);
+    return totalWaves;
+  }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,19 +14,23 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const [owner] = await hre.ethers.getSigners();
+  const WavePortal = await hre.ethers.getContractFactory("WavePortal");
+  const wavePortal = await WavePortal.deploy();
+  await wavePortal.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("WavePortal deployed to:", wavePortal.address);
+  console.log("WavePortal contract owner:", owner.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
     console.error(error);
     process.exit(1);
-  });
+  }
+};
+
+runMain();
